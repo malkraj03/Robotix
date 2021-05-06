17330b5b76ef98c9f7e3c874d3b89dc1a5daddf8
malkraj03 <54039756+malkraj03@users.noreply.github.com>
2021-05-06 17:09:32 +0530

    Update Server.py

--- Flask_Server_Client/Server.py
+++ Flask_Server_Client/Server.py
@@ -5,20 +5,22 @@ app = Flask(__name__)
 print("Server is created...")
 @app.route('/')
 def index():
-    return render_template('index.html', value=10)
+    return render_template('index.html', value=25)
 
-@app.route('/click', methods=['POST', 'GET'])
-def click():
-    var = request.form['sliderValue']
+
+# I've added this method to receive slider updates
+@app.route('/slider_update', methods=['POST', 'GET'])
+def slider():
+    var = request.data
     print(var)
-    clientsocket.send(bytes(var, "utf-8"))
-    return render_template('index.html', value=var)
+    clientsocket.send(bytes(str(var), "utf-8"))
+    return render_template('index.html')
 
 if __name__ == "__main__":
     s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    s.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
-    s.bind( (socket.gethostname(), 5000) )
-    s.listen( 5 )
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+    s.bind((socket.gethostname(), 5000))
+    s.listen(5)
     clientsocket, address = s.accept()
-    print( f"Connection from {address} has been established." )
+    print(f"Connection from {address} has been established.")
     app.run()
