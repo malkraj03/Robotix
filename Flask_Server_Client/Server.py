6ba1f6fff075bdc30108503d83c13cc06266abbb
malkraj03 <54039756+malkraj03@users.noreply.github.com>
2021-05-10 17:10:17 +0530

    Update Server.py

--- Flask_Server_Client/Server.py
+++ Flask_Server_Client/Server.py
@@ -16,6 +16,13 @@ def slider():
     clientsocket.send(bytes(str(var), "utf-8"))
     return render_template('index.html')
 
+@app.route('/slider_act', methods=['POST', 'GET'])
+def slideract():
+    act = request.data
+    print(act)
+    clientsocket.send(bytes(str(act), "utf-8"))
+    return render_template('index.html')
+
 if __name__ == "__main__":
     s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
